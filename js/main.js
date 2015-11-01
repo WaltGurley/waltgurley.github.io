872a2520ff350c53468e4885ca533c712181b942
Walt Gurley <waltgurley@Lauras-MacBook-Air.local>
2015-11-01 16:04:07 -0500

    Rework with p5.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function setup() {
+  var container = getElement('vis-container'),
+  canvas = createCanvas(container.width - 30, windowHeight - 100);
+
+  canvas.parent('vis-container');
+  background(0, 0, 0);
+}
+
+function draw() {
+}
